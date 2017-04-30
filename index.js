bb17f7050cb55e859a4de859592b94ed764fe882
Johan Brattemark <johan.brattemark@gmail.com>
2017-04-29 18:10:21 -0700

    Implement Part 2

--- index.js
+++ index.js
@@ -9,16 +9,41 @@
   let slowLight = document.querySelector('#slowLight');
   let goLight = document.querySelector('#goLight');
 
-  stopButton.addEventListener('click', (event) => {
+  stopButton.addEventListener('click', event => {
     stopLight.classList.toggle("stop");
   });
 
-  slowButton.addEventListener('click', (event) => {
+  slowButton.addEventListener('click', event => {
     slowLight.classList.toggle("slow");
   });
 
-  goButton.addEventListener('click', (event) => {
+  goButton.addEventListener('click', event => {
     goLight.classList.toggle("go");
   });
+
+  stopButton.addEventListener('mouseenter', event => {
+    console.log(`Entered ${event.target.id} button`);
+  });
+
+  stopButton.addEventListener('mouseleave', event => {
+    console.log(`Left ${event.target.id} button`);
+  });
+
+  slowButton.addEventListener('mouseenter', event => {
+    console.log(`Entered ${event.target.id} button`);
+  });
+
+  slowButton.addEventListener('mouseleave', event => {
+    console.log(`Left ${event.target.id} button`);
+  });
+
+  goButton.addEventListener('mouseenter', event => {
+    console.log(`Entered ${event.target.id} button`);
+  });
+
+  goButton.addEventListener('mouseleave', event => {
+    console.log(`Left ${event.target.id} button`);
+  });
+
 
 })();
