fafc1c9caf1814362026e092d6741ba2250ba7e7
Johan Brattemark <johan.brattemark@gmail.com>
2017-04-29 17:10:24 -0700

    Implement Part 1

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  let stopButton = document.querySelector('#stopButton');
+  let slowButton = document.querySelector('#slowButton');
+  let goButton = document.querySelector('#goButton');
+
+  let stopLight = document.querySelector('#stopLight');
+  let slowLight = document.querySelector('#slowLight');
+  let goLight = document.querySelector('#goLight');
+
+  stopButton.addEventListener('click', (event) => {
+    stopLight.classList.toggle("stop");
+  });
+
+  slowButton.addEventListener('click', (event) => {
+    slowLight.classList.toggle("slow");
+  });
+
+  goButton.addEventListener('click', (event) => {
+    goLight.classList.toggle("go");
+  });
+
 })();
